74ada8fb9f276df6a4e4e3b7c5141e60aad7011c
essymaina <essymaina.m@gmail.com>
2023-10-23 11:45:09 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints a statement followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
