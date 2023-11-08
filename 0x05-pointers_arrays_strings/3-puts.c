90d566e804311690323f271d606e576e69da943a
essymaina <essymaina.m@gmail.com>
2023-11-08 12:59:02 +0300

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - Prints a string
@@ -10,7 +11,7 @@ void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(*str);
+		putchar(*str);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
