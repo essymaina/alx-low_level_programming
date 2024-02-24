9cd5a953fbae7842e046cea726dee229aa6c59de
essymaina <essymaina.m@gmail.com>
2024-02-25 00:57:25 +0300

    Create static library from the c files

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -15,22 +15,21 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
-		if (s[c] == 45)
+		if (s[c] == '_')
 		{
-			min *= -2;
+			min *= -1;
 		}
-		while (s[c] >= 48 && s[c] <= 57)
+		if (s[c] >= '0' && s[c] <= '9')
 		{
 			isi = 1;
 			ni = (ni * 10) + (s[c] - '0');
-			c++;
+			
 		}
-		if (isi == 1)
+		else if (isi == 1)
 		{
 			break;
 		}
 		c++;
-		ni *= min;
-		return (ni);
 	}
+	return ni * min;
 }
--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -9,22 +9,27 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int i, n, value;
 
 	value = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		check = 0;
+		int charFound = 0;
 
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (accept[n] == s[i])
 			{
 				value++;
-				check = 1;
+				charFound = 1;
+				break;
 			}
 		}
-		return (0);
+		if (!charFound)
+		{
+			break;
+		}
 	}
+	return value;
 }
